95193ce20c778c96615ea8b9c73636e6c39662a3
Jasongtsk <150237672+Jasongtsk@users.noreply.github.com>
2025-11-27 09:08:15 +0000

    feat: add failiing test for booking creation

--- tests/Feature/BookingCreationTest.php
+++ tests/Feature/BookingCreationTest.php
@@ -8,13 +8,48 @@ use Tests\TestCase;
 
 class BookingCreationTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     */
-    public function test_post_bookings_returns_201_status_code(): void
+    use RefreshDatabase, WithFaker;
+
+    public function test_that_post_bookings_returns_201_status_code(): void
     {
         $response = $this->post('/bookings');
 
         $response->assertStatus(201);
+    }
+
+    public function test_that_post_bookings_returns_correct_json_structure()
+    {
+        $bookingData = [
+            'user_id'      => $this->faker->uuid(),
+            'screening_id' => $this->faker->uuid(),
+            'seats' => [
+                [
+                    'seat_ids' => [
+                        $this->faker->uuid(),
+                        $this->faker->uuid(),
+                    ],
+                    'pricing' => [
+                        'type'       => 'VIP',
+                        'unit_price' => $this->faker->randomFloat(2, 20, 30),
+                    ],
+                ],
+                [
+                    'seat_ids' => [
+                        $this->faker->uuid(),
+                    ],
+                    'pricing' => [
+                        'type'       => 'Regular',
+                        'unit_price' => $this->faker->randomFloat(2, 10, 20),
+                    ],
+                ],
+            ],
+        ];
+
+        $response = $this->post('/bookings', $bookingData);
+
+        $response->assertJsonStructure([
+            'booking_id',
+            'status',
+        ]);
     }
 }
