d8fc21edf217e8be9b572eeaae0b3c6eb24f8651
Jasongtsk <150237672+Jasongtsk@users.noreply.github.com>
2025-12-02 23:31:25 +0000

    test: add failing test that get movie screenings returns correct json structure

---
+++ tests/Feature/RetrieveMovieScreeningsTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Screening;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Str;
+use Tests\TestCase;
+
+class RetrieveMovieScreeningsTest extends TestCase
+{
+    use RefreshDatabase, WithFaker;
+
+    public function test_that_get_movie_screenings_returns_correct_json_structure(): void
+    {
+        $movie_id = Str::uuid();
+
+        Screening::factory(5)
+            ->create([
+                'movie_id' => $movie_id,
+            ]);
+
+        $response = $this->get('/movies/'. $movie_id . '/screenings');
+
+        $response->assertOk();
+
+        $response->assertJsonStructure([
+            'data' => [
+                '*' => [
+                    'id',
+                    'movie_id',
+                    'auditorium_id',
+                    'start_time',
+                    'price'
+                ],
+            ],
+        ]);
+    }
+}
